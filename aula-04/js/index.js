fe6e250f79a016e52aa672d95c783f8ad6119658
Rodrigo Faria <rodrigolfsi@gmail.com>
2021-09-23 22:12:50 -0300

    Add first sample of JavaScript

---
+++ aula-04/js/index.js
@@ -0,0 +1,41 @@
+alert('Ola Mundo!!!')
+
+var username = 'Rodrigo'
+var age = 34
+
+alert(username)
+alert(age)
+
+username = 99
+age = 'Univas'
+
+alert(username)
+alert(age)
+
+let subject = 'Lab IV'
+const university = 'Univas'
+alert(subject)
+alert(university)
+
+let isHoliday = false
+let student = {}
+student = {
+  cpf: '111.111.111-11',
+  email: 'joao@pe_de_feijao.com.br'
+}
+
+let people = []
+people = new Array()
+
+console.log('Estou aprendendo JS')
+console.log(student)
+console.log(student.email)
+console.log(student.cpf)
+
+
+
+
+
+
+
+
