515b4321a553d5c68b419c5656477d814cc55fde
Rodrigo Faria <rodrigolfsi@gmail.com>
2021-10-21 22:10:43 -0300

    Sample of remove element

--- aula-06/js/index.js
+++ aula-06/js/index.js
@@ -6,6 +6,7 @@ const start = () => {
 const saveButtonEvent = () => {
   const inputName = document.getElementById('name')
   addNewGuest(inputName.value)
+  addNewParagraph(inputName.value)
 }
 
 const addNewGuest = guestName => {
@@ -13,8 +14,37 @@ const addNewGuest = guestName => {
   const content = document.createTextNode(guestName)
   li.appendChild(content)
 
+  const removeBtn = document.createElement('input')
+  removeBtn.value = 'Remover'
+  removeBtn.type = 'button'
+  removeBtn.onclick = removeLi
+  li.appendChild(removeBtn)
+  
   const ul = document.getElementById('guest')
   ul.appendChild(li)
 }
+
+const addNewParagraph = paragraphContent => {
+  const paragraph = document.createElement('p')
+  const content = document.createTextNode(paragraphContent)
+  paragraph.appendChild(content)
+
+  const div = document.getElementById('myDiv')
+  const internDiv = div.children[0]
+  internDiv.appendChild(paragraph)
+}
+
+const removeLi = event => {
+  const btn = event.target
+  const li = btn.parentNode
+  console.log(li)
+  const ul = li.parentNode
+  ul.removeChild(li)
+  
+}
+
+
+const divs = document.getElementsByClassName('xpto')
+
 
 start()
