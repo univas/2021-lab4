b91b314f6da5c00e8c6745f46c7d37b9cc91c091
Rodrigo Faria <rodrigolfsi@gmail.com>
2021-12-07 18:54:42 -0300

    Create and delete itens calling API

--- aula-10/js/index.js
+++ aula-10/js/index.js
@@ -2,11 +2,14 @@ const serverUrl = 'https://news-api-node.herokuapp.com/api/v1/news/ea30897c-67fe
 
 const start = () => {
     loadDataFromServer()
+
+    $('#saveButton').click(saveNewPost)
 }
 
 const loadDataFromServer = () => {
     $.get(serverUrl, data => {
         populateList(data)
+        setOnclikButtons()
     })
 }
 
@@ -14,8 +17,49 @@ const populateList = data => {
     $('#messageList').empty()
 
     data.forEach(item => {
-        $('#messageList').append(`<li>${item.post}</li>`)
+        $('#messageList').append(`
+            <li>
+                ${item.post}
+                <input type="button" value="Remover" class="deleteButton"
+                    name="${item.id}">
+            </li>
+        `)
     })
+}
+
+const saveNewPost = () => {
+    const message = {
+        post: $('#message').val()
+    }
+
+    $.post(serverUrl, message)
+        .done(() => {
+            $('#message').val('')
+            loadDataFromServer()
+        })
+}
+
+const setOnclikButtons = () => {
+    $('.deleteButton').click(deletePost)
+}
+
+const deletePost = event => {
+    const deleteButton = event.target
+    $.ajax({
+        url: serverUrl + '/' + deleteButton.name,
+        type: 'DELETE',
+        success: successDelete,
+        error: errorDeletePost,
+    })
+}
+
+const successDelete = () => {
+    alert('Post apagado com sucesso!')
+    loadDataFromServer()
+}
+
+const errorDeletePost = () => {
+    alert('Não foi possível apagar o Post!')
 }
 
 start()
